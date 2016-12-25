6f27d0928ef2b866fec5a5d9be9ba84be0b99e39
tongniu2 <tong.niu2@mail.mcgill.ca>
2016-12-25 01:10:53 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
     $to = "ampersand.chair@mcgillbasic.com";
     $from = $_REQUEST['name'];
-    $headers = "Content-type: text/html;From: $from; Reply-To: $_REQUEST['email']";
+    $headers = "Content-type: text/html;From: $from";
 
     $fields = array();
     $fields["name"] = $_REQUEST['name'];
@@ -13,6 +13,6 @@
     $body .= 'Email : '.$fields['email']. '<br>';
     $body .= 'Message : '.$fields['message']. '<br>';
 
-    $send = mail('ampersand.chair@mcgillbasic.com', $body, $headers);
+    $send = mail($to, $body, $headers);
 
 ?>
