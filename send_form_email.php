88d0200f59cd97bdf176cdeb705ed1e385a204aa
tongniu2 <tong.niu2@mail.mcgill.ca>
2016-12-29 00:37:22 -0500

    Update send_form_email.php

--- send_form_email.php
+++ send_form_email.php
@@ -6,9 +6,9 @@ if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
  
-    $email_to = "you@yourdomain.com";
+    $email_to = "ntong283@gmail.com";
  
-    $email_subject = "Your email subject line";
+    $email_subject = "Testing Testin 123";
  
      
  
